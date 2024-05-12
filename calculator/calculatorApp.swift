e2bb6ccff5f375176248ef452f1f7f38864c2596
vimla kushwaha <vimlakushwaha01@gmail.com>
2024-05-12 09:18:58 +0530

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by vimla kushwaha on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
